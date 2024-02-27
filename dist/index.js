03521a11647645c86adaf6858280a2f437673a94
Shorya <shorya2113081@akgec.ac.in>
2024-02-28 01:10:44 +0530

    fetched the data

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,60 @@
 "use strict";
+const getUsername = document.querySelector("#user");
+const formSubmit = document.querySelector("#form");
+const main = document.querySelector(".main-container");
 async function fetcher(url, options) {
     const response = await fetch(url, options);
     if (!response.ok) {
         throw new Error(`${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     return data;
 }
+const showCard = (indi) => {
+    const { avatar_url, login, url } = indi;
+    main.insertAdjacentHTML("beforeend", `
+        <div class="card">
+            <img src="${avatar_url}" alt="${login}" />
+            <hr/>
+            <div class="card-footer">
+            <img src="${avatar_url}" alt="${login}" />
+            <a href="${url}">Github</a>
+        </div>
+        </div>
+        `);
+};
 function fetchUserData(url) {
-    fetcher(url, {});
+    fetcher(url, {}).then((userInfo) => {
+        for (const indi of userInfo) {
+            showCard(indi);
+        }
+    });
 }
 fetchUserData("https://api.github.com/users");
+getUsername.addEventListener('input', async (e) => {
+    e.preventDefault();
+    const search = getUsername.value.toLowerCase();
+    try {
+        const url = "https://api.github.com/users";
+        const allData = await fetcher(url, {});
+        const match = allData.filter((user) => {
+            return user.login.toLowerCase().includes(search);
+        });
+        main.innerHTML = "";
+        if (match.length === 0) {
+            main.insertAdjacentHTML("beforeend", `<p class="empty-msg">No matching users found.</p>`);
+        }
+        else {
+            for (const user of match) {
+                showCard(user);
+            }
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+formSubmit.addEventListener('submit', async (e) => {
+    e.preventDefault();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+const getUsername= document.querySelector("#user") as HTMLInputElement;
+const formSubmit= document.querySelector("#form") as HTMLFormElement;
+const main= document.querySelector(".main-container") as HTMLElement;
+
 interface UserData {
     id: number;
     login: string;
@@ -18,7 +22,18 @@ async function fetcher<T> (url: string, options?: RequestInit): Promise<T> {
 }
 
 const showCard=(indi:UserData)=>{
-    
+    const {avatar_url, login, url}= indi;
+    main.insertAdjacentHTML("beforeend", `
+        <div class="card">
+            <img src="${avatar_url}" alt="${login}" />
+            <hr/>
+            <div class="card-footer">
+            <img src="${avatar_url}" alt="${login}" />
+            <a href="${url}">Github</a>
+        </div>
+        </div>
+        `
+    );
 }
 function fetchUserData(url : string) {
     fetcher<UserData[]>(url, {}).then((userInfo)=>{
@@ -31,3 +46,31 @@ function fetchUserData(url : string) {
 }
 
 fetchUserData("https://api.github.com/users");
+
+getUsername.addEventListener('input', async (e)=>{
+    e.preventDefault();
+    const search= getUsername.value.toLowerCase();
+    try{
+        const url= "https://api.github.com/users";
+        const allData= await  fetcher<UserData[]>(url, {});
+        const match= allData.filter((user)=>{
+            return user.login.toLowerCase().includes(search)
+        });
+
+        main.innerHTML="";
+        if(match.length===0){
+            main.insertAdjacentHTML("beforeend",`<p class="empty-msg">No matching users found.</p>`);
+        }else{
+            for(const  user of match){
+                showCard(user);
+            }
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+formSubmit.addEventListener('submit', async (e) => {
+    e.preventDefault(); 
+});
